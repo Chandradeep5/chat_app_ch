5f41517776133b36d30814bb874067a4cd20f6ba
Chandra deep <142292192+Chandradeep5@users.noreply.github.com>
2024-05-16 10:39:07 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-app-ch.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
